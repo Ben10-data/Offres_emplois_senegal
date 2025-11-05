19761479a5e530ee29e966e14903f872c4163c66
Ben10-data <mohamed.benomar@ucad.edu.sn>
2025-11-05 16:25:08 +0000

    ajout du script pyspark et du traitement

--- Spark/app/monscript.py
+++ Spark/app/monscript.py
@@ -1,6 +1,6 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col 
-
+from pyspark.sql.functions import col, split
+from pyspark.sql import functions as F 
 
 # creation d'une session spark 
 spark = SparkSession.builder.appName("TestApp").getOrCreate()
@@ -10,7 +10,7 @@ spark.version
 
 # lecture de nos fichier hdfs 
 df = spark.read.parquet("hdfs://namenode:8020/ben/dataLake/")
-
+df = df.dropDuplicates()
 # compter les nombres de lignes qu'on a 
 df.count()
 
@@ -29,23 +29,112 @@ df.printSchema()
 #  |-- date_de_publication: string (nullable = true)
 #  |-- formation: string (nullable = true)
 
+# Les nombres de differents poste et  entreprises existantes 
+df.select("poste").distinct().count()
+
+df.select("entreprise").distinct().count()
 
 #----------------- *** Colonnes a nettoyer et organiser ***-----------------------#
 # Apres un passage de select, exemple la colonne suivant '"entreprise"
 df.select("entreprise").distinct().show(5, truncate=False)
+
+
 
 ###---------------µ*******************************************----------------###
 
 ####     niveau_etude , niveau_experience, contrat_propose
 ####     region, Competence, formation
 
+###------------- traitement de chaque colonne -------------------------------- ###### 
+
+# ---------------- pour la colonne formation 
+
+df_formation = df.withColumn(
+    "formation_clean",
+    F.trim(
+        F.regexp_replace(F.col("formation"), r"</li><li>", ", "))
+)
+
+df_formation = df_formation.withColumn(
+    "formation_clean",
+    F.regexp_replace(F.col("formation_clean"), r"</?li>", "")
+)
+
+df_formation = df_formation.withColumn(
+    "formation_clean",
+    F.regexp_replace(F.col("formation_clean"), r"[\n]", "")
+)
+
+df_formation = df_formation.withColumn(
+    "formation_clean",
+    F.regexp_replace(F.col("formation_clean"),r"</?strong>", "")
+)
+
+df_formation = df_formation.withColumn(
+    "formation_clean",
+    F.trim(F.regexp_replace(F.col("formation_clean"), r"<[^>]+>", ""))
+)
+
+df_formation.drop("formation")
+
+#-------------- Pour la colonne niveau_etude--------------
+
+df_etude = df_formation.withColumn(
+    "niveau_etude_clean",
+    split(col("niveau_etude"), r"\s*(?:,|&|et)\s*")
+)
+
+df_etude = df_etude.drop("niveau_etude")
+df_etude = df_etude.drop("formation")
+
+##------------ contract proposé ---------------------------
+df_contract = df_etude.withColumn(
+    "contract",
+    split(col("contrat_propose"), r"\s*(?:,|&|et)\s*")
+)
+
+df_contract = df_contract.drop("contrat_propose")
+
+### ------------- Pour la region 
+
+df_region = df_contract.withColumn(
+    "region_clean",
+    F.trim(F.regexp_replace(F.col("region"), r"\s*(?:\n|\t)", ""))
+
+)
+
+df_region = df_region.drop("region")
+
+# ----- experience 
+
+df_experience = df_region.withColumn(
+    "experience", 
+    split(col("niveau_experience"), r"\s*(?:,|&|-)\s*")
+)
+
+df_experience = df_experience.drop("niveau_experience")
 
 
-# Voir colonne par colonne, combien de fois une variable est répéter 
-# pour les postes "poste"
-df.groupby("poste").count().orderBy(col("count").desc()).show(10)
+#---------- Traitement des competences 
 
-df.groupBy("entreprise").count().orderBy(col('count').desc()).show(10)
+df_competence = df_experience.withColumn(
+    "competences",
+    split(col("Competence"), r"\s*(-)\s*")
+)
 
+###---------------------------------------------------------------------------------#######""
+#------------------------------------------------------------------------------------#
+# Creation de la premiere dataset qui contient toutes les données 
+
+df_clean = df_competence.drop("Competence")
+
+
+###---------------------------------------------------------------------------------#######""
+#------------------------------------------------------------------------------------#
+# Creation du deuxieme dataset pour le machine learning
+
+#df_ml =
+
+df_clean.show(5, truncate=False)
 
 spark.stop()
