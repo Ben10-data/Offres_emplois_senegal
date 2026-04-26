14b16215406d220a85d17cc574eb15d90e9ac796
Ben 10 <benoma.tamou@gmail.com>
2026-04-26 23:32:22 +0000

    test et deploiement de fastAPI ,ajout des modeles...

--- zenML/src/pipelines/run_pipeline_dash.py
+++ zenML/src/pipelines/run_pipeline_dash.py
@@ -12,11 +12,11 @@ def pipeline_job():
     data = read_data(
         "SELECT * FROM offres_emploi_ml",
         "offres_emploi_ml",
-        "localhost",
-        5493,
-        "datawarehouse",
-        "admin",
-        "admin_pwd"
+        "dpg-d7n2vd1kh4rs73b028eg-a",
+        5432,
+        "offres_emploi_postgres_id4c",
+        "offres_emploi_postgres_id4c_user",
+        "b3HTXg7CxbbMRfeafdVxeJxNJSPa3tkv"
     )
     
     prepared = prepare_data(data)
